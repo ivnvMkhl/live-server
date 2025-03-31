f616b289612c7f1e3f99b83ed811045d06386eb9
root <root@cv4595945.novalocal>
2025-03-31 22:47:25 +0300

    add .gitignore, fix SPA mode

--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
 	"time"
+)
+
+var (
+	port       string
+	src        string
+	spaEntry   string
+	spa        bool
+	logEnabled bool
+)
+
+const (
+	spaEntryDefault   = "/index.html"
+	spaEntryUsage     = "Path to SPA entry file. Default: /index.html"
+	portDefault       = "8080"
+	portUsage         = "Port to run the server on. Default: 8080"
+	srcDefault        = ""
+	srcUsage          = "Relative path to files"
+	spaDefault        = false
+	spaUsage          = "Use server for SPA. Server any route request returned ./index.html"
+	logEnabledDefault = false
+	logEnabledUsage   = "Logging all requests"
 )
 
 const fileMatchRegexStr string = `^\/(.*\/)?.*\.[a-zA-Z0-9?_-]+$`
@@ -17,53 +39,63 @@ func checkFileUrl(url string) bool {
 	return fileMatchRegexp.MatchString(url)
 }
 
-func main() {
-	var (
-		port string
-		src  string
-		spa  bool
-	)
-	const (
-		portDefault = "8080"
-		portUsage   = "Port to run the server on"
-		srcDefault  = ""
-		srcUsage    = "Relative path to files"
-		spaDefault  = false
-		spaUsage    = "Use server for SPA. Server redirects any request to the ./index.html"
-	)
+func logger(logRestrinction bool, logStr string) {
+	if logRestrinction {
+		t := time.Now()
+		formattedTimestamp := fmt.Sprintf("%d/%02d/%02d %02d:%02d:%02d",
+			t.Year(), t.Month(), t.Day(),
+			t.Hour(), t.Minute(), t.Second())
+		log.Println(formattedTimestamp, " ", logStr)
+	}
+}
+
+type httpHandleFunc func(w http.ResponseWriter, r *http.Request)
+
+func spaHandler(basePath string, logEnabled bool) httpHandleFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL.String()
+		isStaticFile := checkFileUrl(url)
+
+		if isStaticFile {
+			logger(logEnabled, "REQUEST: "+url+"  RESPONSE: "+url)
+			http.StripPrefix("/", http.FileServer(http.Dir(basePath))).ServeHTTP(w, r)
+		} else {
+			f, err := os.Open(basePath + spaEntry)
+			if err != nil {
+				logger(logEnabled, "REQUEST: "+url+"  RESPONSE: [Failed] not found"+spaEntry)
+				http.Error(w, "not found "+spaEntry, http.StatusNotFound)
+			} else {
+				logger(logEnabled, "REQUEST: "+url+"  RESPONSE: "+spaEntry)
+				http.ServeContent(w, r, spaEntry, time.Now(), f)
+			}
+		}
+	}
+}
+
+func init() {
 	flag.StringVar(&port, "port", portDefault, portUsage)
 	flag.StringVar(&port, "p", portDefault, portUsage+" (shorthand)")
 	flag.StringVar(&src, "src", srcDefault, srcUsage)
+	flag.StringVar(&spaEntry, "spa-entry", spaEntryDefault, spaEntryUsage)
 	flag.BoolVar(&spa, "spa", spaDefault, spaUsage)
+	flag.BoolVar(&logEnabled, "log", logEnabledDefault, logEnabledUsage)
 	flag.Parse()
+}
 
+func main() {
 	currentDir, err := os.Getwd()
 	if err != nil {
 		log.Fatal("Error getting current directory:", err)
 	}
 
-	filesPath := currentDir + "/" + src
+	fullPath := fmt.Sprintf("%s/%s", currentDir, src)
+
 	if spa {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			url := r.URL.String()
-			isStaticFile := checkFileUrl(url)
-			log.Println(url, isStaticFile)
-			var relativePath string
-			if isStaticFile {
-				relativePath = url
-			} else {
-				relativePath = "/index.html"
-			}
-			f, err := os.Open(filesPath + relativePath)
-			if err != nil {
-				http.Error(w, "not found "+relativePath, http.StatusNotFound)
-			} else {
-				http.ServeContent(w, r, relativePath, time.Now(), f)
-			}
-		})
+		http.HandleFunc("/", spaHandler(fullPath, logEnabled))
 	} else {
-		http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(filesPath))))
+		http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(fullPath))))
 	}
-	log.Println("Starting live on port:", port, "in path:", filesPath, " ...")
+
+	log.Println("Starting live on port:", port, "in path:", fullPath, " ...")
 	http.ListenAndServe(":"+port, nil)
 }
